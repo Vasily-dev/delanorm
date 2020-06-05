35c86581214a5b3e7ad32d3678526776ed821d84
Vasily-dev <guanapok@mail.ru>
2020-06-05 12:58:15 +0300

    Задание 1

--- index.php
+++ index.php
@@ -50,7 +50,7 @@ $show_complete_tasks = rand(0, 1);
                 </nav>
 
                 <a class="button button--transparent button--plus content__side-button"
-                   href="pages/form-project.html" target="project_add">Добавить проект</a>
+                    href="pages/form-project.html" target="project_add">Добавить проект</a>
             </section>
 
             <main class="content__main">
@@ -72,7 +72,12 @@ $show_complete_tasks = rand(0, 1);
 
                     <label class="checkbox">
                         <!--добавить сюда атрибут "checked", если переменная $show_complete_tasks равна единице-->
-                        <input class="checkbox__input visually-hidden show_completed" type="checkbox">
+                        <input class="checkbox__input visually-hidden show_completed" type="checkbox"
+                        <?php 
+                            if ($show_complete_tasks == 1) {
+                                echo 'checked';
+                            }
+                        ?>>
                         <span class="checkbox__text">Показывать выполненные</span>
                     </label>
                 </div>
@@ -93,6 +98,21 @@ $show_complete_tasks = rand(0, 1);
                         <td class="task__date"></td>
                     </tr>
                     <!--показывать следующий тег <tr/>, если переменная $show_complete_tasks равна единице-->
+                    <?php 
+                        $showtask = '<tr class="tasks__item task task--completed">
+                        <td class="task__select">
+                            <label class="checkbox task__checkbox">
+                                <input class="checkbox__input visually-hidden" type="checkbox" checked>
+                                <span class="checkbox__text">Записаться на интенсив "Базовый PHP"</span>
+                            </label>
+                        </td>
+                        <td class="task__date">10.10.2019</td>
+                        <td class="task__controls"></td>
+                    </tr>';
+                        if ($show_complete_tasks == 1) {
+                            echo "$showtask";
+                        }
+                    ?>
                 </table>
             </main>
         </div>
